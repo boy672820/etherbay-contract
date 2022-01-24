1e1d85e5325c66efb0fc3f71c07c2eef401f1261
이선주 <seondev@iseonjuui-MacBookPro.local>
2022-01-24 23:56:15 +0900

    ownership 상속 추가

--- contracts/ProductOwnership.sol
+++ contracts/ProductOwnership.sol
@@ -1,14 +1,25 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import './ProductFactory.sol';
+import "./ProductFactory.sol";
 
-contract ProductOwnership is ProductFactory {
-  function balanceOf(address _owner) public view returns (uint256) {
-    return ownerProductCount[_owner];
-  }
+interface Ownership {
+    function balanceOf(address _owner) external view returns (uint256);
 
-  function ownerOf(uint256 _id) public view returns (address) {
-    return productToOwner[_id];
-  }
+    function ownerOf(uint256 _id) external view returns (address);
+}
+
+contract ProductOwnership is ProductFactory, Ownership {
+    function balanceOf(address _owner)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return ownerProductCount[_owner];
+    }
+
+    function ownerOf(uint256 _id) external view override returns (address) {
+        return productToOwner[_id];
+    }
 }
--- test/ProductFactory.test.js
+++ test/ProductFactory.test.js
@@ -3,8 +3,8 @@ const { ethers } = require('hardhat');
 
 describe('ProductFactory', async function () {
   let owner, user;
-  let productFactory;
-  let newProductEvents = [];
+  let productOwnership;
+  let newProductEvents = [], newProductId;
 
   const product = [
     'MacBook-pro M1X 16inch',
@@ -15,19 +15,19 @@ describe('ProductFactory', async function () {
 
   before(async function () {
     const [ownerSigner, addrSigner] = await ethers.getSigners();
-
     owner = ownerSigner;
     user = addrSigner;
 
-    const Factory = await ethers.getContractFactory('ProductFactory');
-    const factory = await Factory.deploy();
-    await factory.deployed();
+    const Ownership = await ethers.getContractFactory('ProductOwnership');
+    const ownership = await Ownership.deploy();
 
-    productFactory = factory;
+    await ownership.deployed();
+
+    productOwnership = ownership;
   });
 
   it('#1 상품 생성하기', async function () {
-    const createProduct = await productFactory.connect(user).createProduct(...product);
+    const createProduct = await productOwnership.connect(user).createProduct(...product);
 
     const receipt = await createProduct.wait();
 
@@ -42,21 +42,20 @@ describe('ProductFactory', async function () {
   it('#2 추가된 상품 조회하기', async function () {
     const lastEvent = newProductEvents.pop();
     const productId = lastEvent.args.id.toNumber();
+    newProductId = productId;
 
-    const [...hot] = await productFactory.getProduct(productId);
+    const [...hot] = await productOwnership.getProduct(productId);
     const productOwner = hot.pop();
 
     expect(hot).deep.to.equal(product);
     expect(productOwner).to.equal(user.address);
   });
 
-  // it('#3 BalanceOf and ownerOf', async function () {
-  //   const Ownership = await ethers.getContractFactory('ProductOwnership');
-  //   const ownership = await Ownership.deploy();
-  //   await ownership.deployed();
+  it('#3 Ownable 체크', async function () {
+    const balanceOf = await productOwnership.balanceOf(user.address);
+    const ownerOf = await productOwnership.ownerOf(newProductId);
 
-  //   const balance = await ownership.balanceOf(user.address);
-
-  //   console.log(balance);
-  // });
+    expect(balanceOf.toNumber()).to.equal(1);
+    expect(ownerOf).to.equal(user.address);
+  });
 });
