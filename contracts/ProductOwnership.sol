b0d8b23406e39248a291cd76e4b7addb96b14874
이선주 <seondev@iseonjuui-MacBookPro.local>
2022-01-27 23:42:58 +0900

    transfer 구현 중

--- contracts/ProductOwnership.sol
+++ contracts/ProductOwnership.sol
@@ -1,25 +1,50 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./ProductFactory.sol";
+import './ProductFactory.sol';
 
-interface Ownership {
-    function balanceOf(address _owner) external view returns (uint256);
+contract ProductOwnership is ProductFactory {
+    event Transfer(address from, address to, uint256 tokenId);
+    event Approval(address productToOwner, address to, uint256 tokenId);
 
-    function ownerOf(uint256 _id) external view returns (address);
-}
+    mapping(uint256 => address) private _approvals;
 
-contract ProductOwnership is ProductFactory, Ownership {
-    function balanceOf(address _owner)
-        external
-        view
-        override
-        returns (uint256)
-    {
+    function balanceOf(address _owner) public view returns (uint256) {
         return ownerProductCount[_owner];
     }
 
-    function ownerOf(uint256 _id) external view override returns (address) {
+    function ownerOf(uint256 _id) public view returns (address) {
         return productToOwner[_id];
     }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external {}
+
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) private {
+        ownerProductCount[_from]--;
+        ownerProductCount[_to]++;
+
+        productToOwner[_tokenId] = _to;
+
+        emit Transfer(_from, _to, _tokenId);
+    }
+
+    function _approve(address _to, uint256 _tokenId) private {
+        _approvals[_tokenId] = _to;
+
+        emit Approval(this.ownerOf(_tokenId), _to, _tokenId);
+    }
+
+    function _isApprovedOrOwner(address _spender, uint256 _tokenId)
+        internal
+        view
+        returns (bool)
+    {}
 }
