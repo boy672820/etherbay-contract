f769e9afe022e3b6eca1259385c71893ad7b809c
이선주 <seondev@iseonjuui-MacBookPro.local>
2022-03-01 17:11:31 +0900

    erc721enumerable 확장 적용

--- contracts/EtherbayProduct.sol
+++ contracts/EtherbayProduct.sol
@@ -6,18 +6,53 @@ import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract EtherbayProduct is ERC721URIStorage, Ownable {
+contract EtherbayProduct is ERC721URIStorage, ERC721Enumerable, Ownable {
   using Counters for Counters.Counter;
 
   Counters.Counter private _tokenIds;
 
   constructor() ERC721('Etherbay Product', 'EBP') {}
 
-  function mint(address recipient, string calldata tokenURI) public onlyOwner {
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 tokenId
+  ) internal override(ERC721, ERC721Enumerable) {
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
+
+  function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    super._burn(tokenId);
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    override(ERC721, ERC721URIStorage)
+    returns (string memory)
+  {
+    return super.tokenURI(tokenId);
+  }
+
+  function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721, ERC721Enumerable)
+    returns (bool)
+  {
+    return super.supportsInterface(interfaceId);
+  }
+
+  // ---------------------------------------------------------------------------
+
+  function mint(address _recipient, string calldata _tokenURI)
+    public
+    onlyOwner
+  {
     _tokenIds.increment();
 
     uint256 newTokenId = _tokenIds.current();
-    _mint(recipient, newTokenId);
-    _setTokenURI(newTokenId, tokenURI);
+    _mint(_recipient, newTokenId);
+    _setTokenURI(newTokenId, _tokenURI);
   }
 }
--- test/EtherbayProduct.test.js
+++ test/EtherbayProduct.test.js
@@ -2,13 +2,14 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('EtherbayProduct', async function () {
-  let owner, user;
+  let owner, user, user2;
   let product;
 
   before(async function () {
-    const [ownerSigner, userSigner] = await ethers.getSigners();
+    const [ownerSigner, userSigner, userSigner2] = await ethers.getSigners();
     owner = ownerSigner;
     user = userSigner;
+    user2 = userSigner2;
 
     const Product = await ethers.getContractFactory('EtherbayProduct');
     product = await Product.deploy();
@@ -21,13 +22,41 @@ describe('EtherbayProduct', async function () {
     expect([name, symbol]).to.deep.equal(['Etherbay Product', 'EBP']);
   });
 
+  const jsonUri =
+    'https://gateway.pinata.cloud/ipfs/Qmddr94PsC89ENMnEBk6WhxxBP8cbDGYNfeAJscqgwRCSM';
+
   it('#1 토큰 발급 테스트', async function () {
-    const tx = await product.mint(user.address, 'test');
+    const tx = await product.mint(user.address, jsonUri);
     const receipt = await tx.wait();
     const { tokenId } = receipt.events[0].args;
 
+    // 소유자 확인
     const owner = await product.ownerOf(tokenId);
-
     expect(owner).to.equal(user.address);
+
+    // 토큰 uri 확인
+    const uri = await product.tokenURI(tokenId);
+    expect(uri).to.equal(jsonUri);
+  });
+
+  it('#2 ERC721 Enumerable', async function () {
+    await Promise.all([
+      product.mint(user.address, jsonUri),
+      product.mint(user.address, jsonUri),
+      product.mint(user.address, jsonUri),
+    ]);
+
+    const balance = await product.balanceOf(user.address);
+    let total = balance.toNumber(),
+      ownerTokens = [];
+
+    while (total > 0) {
+      total -= 1;
+
+      const tokenId = await product.tokenOfOwnerByIndex(user.address, total);
+      ownerTokens.push(tokenId);
+    }
+
+    expect(ownerTokens).to.be.a('array');
   });
 });
